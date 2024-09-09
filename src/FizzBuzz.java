2398458b879451e5617c1e30b5041beb33259640
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-09-09 09:43:42 -0400

    Refactored

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,32 +3,64 @@
  */
 class FizzBuzz {
 
+    // public static void main(String[] args) {
+
+    //     for (int i = 1; i < 100; i++) {
+
+    //         // Find out which numbers divide i.
+    //         boolean divisibleBy3 = i % 3 == 0;
+    //         boolean divisibleBy5 = i % 5 == 0;
+
+    //         // Print our appropriate result.
+    //         if (divisibleBy3 && divisibleBy5) {
+
+    //             System.out.println("Fizz Buzz");
+
+    //         } else if (divisibleBy3) {
+
+    //             System.out.println("Fizz");
+
+    //         } else if (divisibleBy5) {
+
+    //             System.out.println("Buzz");
+
+    //         } else {
+
+    //             System.out.println(i);
+
+    //         }
+    //     }
+    // }
     public static void main(String[] args) {
+        int i = 1;
+        while (i < 100) {
+            numberCalculation(i);
+            ++i;
 
-        for (int i = 1; i < 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+            
         }
     }
+
+    private static void numberCalculation(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+    }
+
 }
